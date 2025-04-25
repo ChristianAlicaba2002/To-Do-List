70bbfd477400a9384581209a1c87a62b75ce1597
Christian <chirstiandave120702@gmail.com>
2025-04-25 20:58:05 +0800

    Initialize Models

---
+++ backend/app/Models/Client.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class Client extends Authenticatable
+{
+    use HasFactory, Notifiable , HasApiTokens;
+
+    protected $table = 'client';
+    protected $primary_key = 'user_id';
+    protected $fillable = [
+        'user_id',
+        'username',
+        'email',
+    ];
+
+    protected $hidden = [
+        'password',
+    ];
+}
